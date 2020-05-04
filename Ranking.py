bde18e62d6e5f95ebcd39c379100719dee1225da
congcoding <congcoding@gmail.com>
2020-05-04 17:27:43 +0900

    게임 1 로컬 랭킹 다시 수정

--- Ranking.py
+++ Ranking.py
@@ -43,7 +43,8 @@ def ranking_screen():
     if length > 4:
         length = 4
     for i in range(0, length):
-        draw_text(str(PySpaceshipLocalRankingList[i]), default_font, screen, 120, 180 + (i * 30),  BLACK)
+        draw_text(str(PySpaceshipLocalRankingList[i][0]), default_font, screen, 90, 180 + (i * 30),  BLACK)
+        draw_text(str(PySpaceshipLocalRankingList[i][1]), default_font, screen, 140, 180 + (i * 30),  BLACK)
     # DB에서 점수 정보 가져오기
     draw_text("우주에서 살아남기(Online)", default_font, screen, 360, 150,  BLACK)
     try :
